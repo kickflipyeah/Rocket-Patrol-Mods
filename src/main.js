cb999998986fa1afc2d31ab3309ac7acd599291b
kickflipyeah <59582785+kickflipyeah@users.noreply.github.com>
2023-05-04 05:43:02 -0700

    please work

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+/*
+ *  Jessica Zogaric
+ *  Rocket Patrol: The Cute Remaster
+ *  Approximate time to complete: 15 1/2 hours
+ *  Mods:
+ *  Background music (5), High score counter (5), "FIRE" UI (5), New scrolling tile for background (5),
+ *  new title screen (10), add 4 randomly playing sound effects (10), parallax background scrolling(10),
+ *  mouse controls direction and shooting(15), add new spaceship with new functions and art (15),
+ *  player can move after rocket fires(5)
+ *  Point total: 85
+ *  Citations: background music by Lesiakower on pixabay.com, Phaser documentation
+ */
+
 //configured the screen so its 640 x 480 and pass the config thru the let game
 let config = {
     type: Phaser.AUTO,
